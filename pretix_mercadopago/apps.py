5a69729e17471d364523dd40123566339ff658c6
cmotadev <cmota.dev@gmail.com>
2024-06-02 08:32:17 -0300

    Porting to django 4.2.x

---
+++ pretix_mercadopago/apps.py
@@ -0,0 +1,38 @@
+from django.utils.functional import cached_property
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginMercadoPago(PluginConfig):
+    name = 'pretix_mercadopago'
+    verbose_name = 'MercadoPago Pretix plugin'
+    default = True
+
+    class PretixPluginMeta:
+        name = gettext_lazy('MercadoPago Pretix plugin')
+        author = 'FOSS4G team'
+        description = gettext_lazy('Payment Provider for MercadoPago.')
+        visible = True
+        version = __version__
+        category = 'PAYMENT'
+        compatibility = "pretix>=2.7.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+    def installed(self, event):
+        pass  # Your code here
+
+    @cached_property
+    def compatibility_errors(self):
+        errs = []
+        try:
+            import mercadopago  # NOQA
+        except ImportError:
+            errs.append("Python package 'mercadopago' SDK is not installed.")
+        return errs
+
